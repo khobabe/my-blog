6bf691a61a707c79291cd948d7b49a719627765a
khobabe <khobabe007@gmail.com>
2024-11-12 16:26:25 +0530

    first comit. have installed and done the setups

--- src/app/(frontend)/layout.js
+++ src/app/(frontend)/layout.js
@@ -6,7 +6,7 @@ const geistSans = localFont({
   variable: "--font-geist-sans",
   weight: "100 900",
 });
-const geistMono = localFont({
+const GeistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
---
+++ src/app/(frontend)/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>hello, this is the home page</div>
+  )
+}
+
+export default page 
